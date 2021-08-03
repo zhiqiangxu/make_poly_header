06cee6f25771e87b47979d29503ca0e5e0aa1906
徐志强 <652732310@qq.com>
2021-08-03 16:14:00 +0800

    use GetPasswd

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/zhiqiangxu/make_poly_header
 go 1.15
 
 require (
+	github.com/howeyc/gopass v0.0.0-20190910152052-7cb4b85ec19c
 	github.com/polynetwork/poly v0.0.0-20210112063446-24e3d053e9d6
 	github.com/polynetwork/poly-go-sdk v0.0.0-20210114120411-3dcba035134f
 	github.com/polynetwork/poly-io-test v0.0.0-20200819093740-8cf514b07750 // indirect
--- main.go
+++ main.go
@@ -2,25 +2,29 @@ package main
 
 import (
 	"encoding/hex"
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"strconv"
 	"strings"
 
+	"github.com/howeyc/gopass"
 	sdk "github.com/polynetwork/poly-go-sdk"
 	"github.com/polynetwork/poly/common"
+	"github.com/polynetwork/poly/core/types"
 )
 
 var heightRoots string
+var heightSigs string
 var rpc string
 var walletPath string
-var psw string
 
 func init() {
 	flag.StringVar(&heightRoots, "heightRoots", "", "specify poly heights and roots")
+	flag.StringVar(&heightSigs, "heightSigs", "", "specify poly heights and sigs")
 	flag.StringVar(&rpc, "rpc", "", "specify poly rpc url")
 	flag.StringVar(&walletPath, "wallet", "", "specify wallet path")
-	flag.StringVar(&psw, "psw", "", "specify wallet password")
 
 	flag.Parse()
 }
@@ -35,6 +39,55 @@ func setUpPoly(polySdk *sdk.PolySdk, rpcAddr string) error {
 	return nil
 }
 
+func processHeightSigns() {
+	heightSigArray := strings.Split(heightSigs, ",")
+
+	var (
+		sigMaps []map[uint64]string
+		orders  []uint64
+	)
+	for _, f := range heightSigArray {
+		sigBytes, err := ioutil.ReadFile(f)
+		if err != nil {
+			panic(err)
+		}
+		m := make(map[uint64]string)
+		lines := strings.Split(string(sigBytes), "\n")
+		first := len(orders) == 0
+		for _, line := range lines {
+			cleaned := strings.TrimSpace(line)
+			parts := strings.Split(cleaned, ":")
+			if len(parts) != 2 {
+				panic("invalid sig format")
+			}
+			height, err := strconv.ParseUint(parts[0], 10, 64)
+			if err != nil {
+				panic(err)
+			}
+			m[height] = parts[1]
+			if first {
+				orders = append(orders, height)
+			}
+		}
+		sigMaps = append(sigMaps, m)
+	}
+	combinedSigs := make(map[uint64][]string)
+	var result []string
+	for _, height := range orders {
+		for i := 0; i < len(sigMaps); i++ {
+			sig, ok := sigMaps[i][height]
+			if !ok {
+				panic(fmt.Sprintf("sig missing for height %d", height))
+			}
+			combinedSigs[height] = append(combinedSigs[height], sig)
+		}
+		sigsForHeight := strings.Join(combinedSigs[height], ":")
+		result = append(result, fmt.Sprintf("%d:%s", height, sigsForHeight))
+	}
+
+	heightSigs = strings.Join(result, ",")
+}
+
 func main() {
 	polySdk := sdk.NewPolySdk()
 	err := setUpPoly(polySdk, rpc)
@@ -42,18 +95,34 @@ func main() {
 		panic(err)
 	}
 
-	wallet, err := polySdk.OpenWallet(walletPath)
-	if err != nil {
-		panic(err)
-	}
-	signer, err := wallet.GetDefaultAccount([]byte(psw))
-	if err != nil {
-		panic(err)
+	var signer *sdk.Account
+	if heightSigs == "" {
+		wallet, err := polySdk.OpenWallet(walletPath)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("Password: ")
+		psw, err := gopass.GetPasswd()
+		if err != nil {
+			panic(err)
+		}
+		signer, err = wallet.GetDefaultAccount([]byte(psw))
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		processHeightSigns()
 	}
 
 	heightRootArray := strings.Split(heightRoots, ",")
+	heightSigArray := strings.Split(heightSigs, ",")
+	if heightSigs != "" && len(heightSigArray) != len(heightRootArray) {
+		panic("root and sig not match")
+	}
 
-	for _, heightRoot := range heightRootArray {
+	headers := make(map[uint32]*types.Header)
+
+	for i, heightRoot := range heightRootArray {
 		heightRootParts := strings.Split(heightRoot, ":")
 		if len(heightRootParts) != 2 {
 			panic(fmt.Sprintf("invalid heightRoot:%s", heightRoot))
@@ -71,13 +140,42 @@ func main() {
 			panic(err)
 		}
 
-		blkHash := hdr.Hash()
-		sig, err := signer.Sign(blkHash[:])
+		if heightSigs == "" {
+			blkHash := hdr.Hash()
+			sig, err := signer.Sign(blkHash[:])
+			if err != nil {
+				panic(err)
+			}
+
+			fmt.Printf("%d:%s\n", heightInt, hex.EncodeToString(sig))
+		} else {
+			heightSigParts := strings.Split(heightSigArray[i], ":")
+			heightIntSig, err := strconv.ParseUint(heightSigParts[0], 10, 64)
+			if err != nil {
+				panic(err)
+			}
+			if heightInt != heightIntSig {
+				panic("height mismatch")
+			}
+			hdr.SigData = nil
+			for i := 1; i < len(heightSigParts); i++ {
+				sig, err := hex.DecodeString(heightSigParts[i])
+				if err != nil {
+					panic(err)
+				}
+				hdr.SigData = append(hdr.SigData, sig)
+			}
+			headers[hdr.Height] = hdr
+		}
+
+	}
+
+	if heightSigs != "" {
+		headerJson, err := json.Marshal(headers)
 		if err != nil {
 			panic(err)
 		}
-
-		fmt.Printf("signature for height %d: %s\n", heightInt, hex.EncodeToString(sig))
+		ioutil.WriteFile("headers.json", headerJson, 0777)
 	}
 
 }
